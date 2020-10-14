fa99495b2ad034b907bedfeb4cd58f89cff805b1
jin.li <78626028@qq.com>
2020-10-14 20:12:44 +0800

    调试移动方法缩小

--- Assets/SVG Importer/Example Projects/TestSVG/Scripts/GameCameraCtrl.cs
+++ Assets/SVG Importer/Example Projects/TestSVG/Scripts/GameCameraCtrl.cs
@@ -16,16 +16,18 @@ public class GameCameraCtrl : MonoBehaviour
  
     float distanceScale;
 
-    Vector3 pos1;
-    Vector3 pos2;
+    Vector3 zoomTouchPos1;
+    Vector3 zoomTouchPos2;
+    bool zoomTouchPos1Flag = false;
+    bool zoomTouchPos2Flag = false;
 
-    const float SCALEPOSTION = 0.000001f; // 全局位置缩放大小
     bool touchBeg = false;
 
     void Start()
     {
         SCREEN_HEIGHT = Screen.height;
         SCREEN_WIDTH = Screen.width;
+        Input.multiTouchEnabled = true;
         mCamera = this.GetComponent<Camera>();
         if (mCamera == null) {
             Debug.LogError("need camera component");
@@ -34,126 +36,129 @@ public class GameCameraCtrl : MonoBehaviour
  
     void Update()
     {
-        if (touchBeg)
-        {
+        if (touchBeg) {
             timeTouch += Time.deltaTime;
         }
-        // -------------- for mobile touch input ----------------------
+        // -------------- for mobile touch input start ----------------------
         if (Input.touchCount == 1)
         {
+            Debug.Log("touch slide - 0");
             var data = Input.GetTouch(0);
-            this.Slide(data.deltaPosition * SCALEPOSTION);
-            pos1 = Vector3.zero;
-            pos2 = Vector3.zero;
+            this.Slide(data.deltaPosition / mCamera.orthographicSize / 100f);
+            zoomTouchPos1 = Vector3.zero;
+            zoomTouchPos2 = Vector3.zero;
             distanceScale = 0f;
-            if (data.phase == TouchPhase.Began)
-            {
+            zoomTouchPos1Flag = false;
+            zoomTouchPos2Flag = false;
+            if (data.phase == TouchPhase.Began) {
                 StopAllCoroutines();
-                touch_beg = data.deltaPosition * SCALEPOSTION;
+                touch_beg = data.deltaPosition;
                 timeTouch = 0.000001f;
                 touchBeg = true;
+                zoomTouchPos1Flag = true;
+                zoomTouchPos1 = data.position;
+                distanceScale = Vector3.Distance(zoomTouchPos1, zoomTouchPos2);
+                Debug.Log("touch zoom - 0");
             }
-            if (data.phase == TouchPhase.Ended)
-            {
+            if (data.phase == TouchPhase.Ended) {
                 touchBeg = false;
                 this.StartAutoSlide(touch_beg, data.position);
             }
         }
         else if (Input.touchCount == 2)
         {
+            Debug.Log("touch zoom - 1");
             touchBeg = false;
             var d1 = Input.GetTouch(0);
             var d2 = Input.GetTouch(1);
-            if (d1.phase == TouchPhase.Began)
-            {
-                pos1 = d1.position;
-                distanceScale = Vector3.Distance(pos1, pos2);
+            if (d1.phase == TouchPhase.Began && zoomTouchPos1Flag == false) {
+                zoomTouchPos1Flag = true;
+                zoomTouchPos1 = d1.position;
+                distanceScale = Vector3.Distance(zoomTouchPos1, zoomTouchPos2);
+                Debug.Log("touch zoom - 2");
             }
-            if (d2.phase == TouchPhase.Began)
-            {
-                pos2 = d2.position;
-                distanceScale = Vector3.Distance(pos1, pos2);
+            if (d2.phase == TouchPhase.Began) {
+                zoomTouchPos2Flag = true;
+                zoomTouchPos2 = d2.position;
+                distanceScale = Vector3.Distance(zoomTouchPos1, zoomTouchPos2);
+                Debug.Log("touch zoom - 3");
             }
-            if (pos2 != Vector3.zero && pos2 != Vector3.zero)
-            {
-                float dis = Vector3.Distance(pos1, pos2);
-                if (d1.phase == TouchPhase.Moved || d2.phase == TouchPhase.Moved)
-                {
-                    if (dis > distanceScale)
-                    {
-                        this.ZoomOut(Time.deltaTime * 100f);
-                    }
-                    else if (dis < distanceScale)
-                    {
-                        this.ZoomIn(Time.deltaTime * 100f);
+            Debug.Log("touch zoom - 4" + ", d1.phase = " + d1.phase + ", d2.phase = " + d2.phase);
+            Debug.Log("zoomTouchPos1Flag = " + zoomTouchPos1Flag + ", zoomTouchPos2Flag = " + zoomTouchPos2Flag);
+            if (zoomTouchPos1Flag == true && zoomTouchPos2Flag == true) {
+                float dis = Vector3.Distance(zoomTouchPos1, zoomTouchPos2);
+                Debug.Log("touch zoom - 5" + "dis = " + dis + ", dis2 = " + distanceScale);
+                if (d1.phase == TouchPhase.Moved || d2.phase == TouchPhase.Moved) {
+                    if (dis > distanceScale) {
+                        Debug.Log("touch zoom - 6");
+                        this.ZoomOut(Time.deltaTime * 50f);
+                    } else if (dis < distanceScale) {
+                        Debug.Log("touch zoom - 7");
+                        this.ZoomIn(Time.deltaTime * 50f);
                     }
                 }
                 distanceScale = dis;
             }
-        }
-        else
-        {
+        } else {
             touchBeg = false;
-            pos1 = Vector3.zero;
-            pos2 = Vector3.zero;
+            zoomTouchPos1 = Vector3.zero;
+            zoomTouchPos2 = Vector3.zero;
             distanceScale = 0f;
+            zoomTouchPos1Flag = false;
+            zoomTouchPos2Flag = false;
+            Debug.Log("touch reset.");
         }
- 
-        //--------------- for pc mouse input ------------------
-        //slide
-        if (Input.GetMouseButton(0))
-        {
-            if (lastMousePos == Vector2.zero)
-            {
+        // -------------- for mobile touch input start end----------------------
+
+        //--------------- for pc mouse input start ------------------
+        // slide
+        if (Input.GetMouseButton(0)) {
+            if (lastMousePos == Vector2.zero) {
                 timeTouch = 0.000001f;
                 StopAllCoroutines();
                 lastMousePos = Input.mousePosition;
                 touch_beg = lastMousePos;
                 return;
-            }
-            else
-            {
+            } else {
                 timeTouch += Time.deltaTime;
                 Vector2 pos = Input.mousePosition;
-                this.Slide(0.015f * (pos - lastMousePos));
+                this.Slide((pos - lastMousePos) / mCamera.orthographicSize / 100f);
                 lastMousePos = pos;
             }
-        }
-        else
-        {
+        } else {
             if (lastMousePos != Vector2.zero) {
                 this.StartAutoSlide(touch_beg, Input.mousePosition);
             }
             lastMousePos = Vector2.zero;
         }
-        //----------------------for pc zoom
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
-        {
+        // zoom out
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) {
             Debug.Log("Time.deltaTime = " + Time.deltaTime);
-            this.ZoomOut(Time.deltaTime * 100f);
+            this.ZoomOut(Time.deltaTime * 50f);
             this.Slide(0f, 0f);
         }
-        //Zoom in
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
-        {
-            this.ZoomIn(Time.deltaTime * 100f);
+        // Zoom in
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+            this.ZoomIn(Time.deltaTime * 50f);
             this.Slide(0f, 0f);
         }
+        //--------------- for pc mouse input end ------------------
     }
  
     float maxtime = 3f;
     //开始惯性动画
     void StartAutoSlide(Vector2 orign, Vector2 ended)
     {
-        if (Mathf.Abs(timeTouch) < 0.01f)
-        {
+        if (Mathf.Abs(timeTouch) < 0.01f) {
             return;
         }
+
         maxtime = 3f;
         StopAllCoroutines();
-        Debug.Log("slide map ,speed = " + (Vector2.Distance(orign, ended) / timeTouch / 1000f) + "  touchTime=" + timeTouch + " posended" + ended + "  beg" + orign);
+        // Debug.Log("slide map: speed = " + (Vector2.Distance(orign, ended) / timeTouch / mCamera.orthographicSize / 100f));
+        // Debug.Log("slide map: touchTime = " + timeTouch + ", posended = " + ended + ", posbeg = " + orign);
  
-        StartCoroutine(RunSliderAction(ended.x - orign.x, ended.y - orign.y, Vector2.Distance(orign, ended) / timeTouch / 1000f));
+        StartCoroutine(RunSliderAction((ended.x - orign.x) / mCamera.orthographicSize / 100f, (ended.y - orign.y) / mCamera.orthographicSize / 100f, Vector2.Distance(orign, ended) / timeTouch / mCamera.orthographicSize / 100f));
         timeTouch = 0.000001f;
     }
     Vector2 touch_beg;
@@ -167,7 +172,8 @@ public class GameCameraCtrl : MonoBehaviour
             yield return new WaitForEndOfFrame();
             time += Time.deltaTime;
             dis += Time.deltaTime * 0.01f;
-            speed -= Time.deltaTime * 5f;
+            speed -= Time.deltaTime * 30f;
+            // Debug.Log("speed = " + speed + ", dx = " + dx + ", dy = " + dy);
             this.Slide(Time.deltaTime * speed * dx, dy * Time.deltaTime * speed);
         }
     }
@@ -188,22 +194,28 @@ public class GameCameraCtrl : MonoBehaviour
         pos.y += dy;
         transform.position = pos;
  
-        //实际宽度
-        float real_width = mCamera.orthographicSize * 2 * SCREEN_WIDTH / SCREEN_HEIGHT;
-        float real_height = real_width * SCREEN_HEIGHT / SCREEN_WIDTH;
+        // 实际宽度 unity单位大小 = 分辨率 / 摄像机size / 100（单位像素比）
+        float real_unit_width = SCREEN_WIDTH / mCamera.orthographicSize / 100f;
+        float real_unit_height = SCREEN_HEIGHT / mCamera.orthographicSize / 100f;
+
+        // Debug.Log("real_unit_width = " + real_unit_width + ", real_unit_height = " + real_unit_height);
  
         //process edge
-        if (transform.position.x < -(max_allow_width - real_width / 2f)) {
-            this.SetPosX(-max_allow_width);
+        float leftMinPosX = -(max_allow_width - real_unit_width / 2f);
+        if (transform.position.x < leftMinPosX) {
+            this.SetPosX(leftMinPosX);
         }
-        if (transform.position.x > max_allow_width - real_width / 2f) {
-            this.SetPosX(max_allow_width - real_width);
+        float rightMaxPosX = max_allow_width - real_unit_width / 2f;
+        if (transform.position.x > rightMaxPosX) {
+            this.SetPosX(rightMaxPosX);
         }
-        if (transform.position.y < -(max_allow_height - real_height / 2f)) {
-            this.SetPosY(-max_allow_height);
+        float topMinPosY = -(max_allow_height - real_unit_height / 2f);
+        if (transform.position.y < topMinPosY) {
+            this.SetPosY(topMinPosY);
         }
-        if (transform.position.y > max_allow_height - real_height / 2f) {
-            this.SetPosY(max_allow_height - real_height);
+        float bottomMaxPosY = max_allow_height - real_unit_height / 2f;
+        if (transform.position.y > bottomMaxPosY) {
+            this.SetPosY(bottomMaxPosY);
         }
     }
     //放大接口
@@ -227,18 +239,22 @@ public class GameCameraCtrl : MonoBehaviour
     // 设置x偏移量，偏移量是从世界坐标原点开始计算
     void SetPosX(float offsetX)
     {
-        float real_width = mCamera.orthographicSize * 2 * SCREEN_WIDTH / SCREEN_HEIGHT;
-        float real_height = real_width * SCREEN_HEIGHT / SCREEN_WIDTH;
+        // float real_width = mCamera.orthographicSize * 2 * SCREEN_WIDTH / SCREEN_HEIGHT;
+        // float real_height = real_width * SCREEN_HEIGHT / SCREEN_WIDTH;
+        // var posOld = transform.position;
+        // transform.position = new Vector3(real_width / 2f + offsetX, posOld.y, posOld.z);
         var posOld = transform.position;
-        transform.position = new Vector3(real_width / 2f + offsetX, posOld.y, posOld.z);
+        transform.position = new Vector3(offsetX, posOld.y, posOld.z);
     }
     // 设置y偏移量，偏移量是从世界坐标原点开始计算
     void SetPosY(float offsetY)
     {
-        float real_width = mCamera.orthographicSize * 2 * SCREEN_WIDTH / SCREEN_HEIGHT;
-        float real_height = real_width * SCREEN_HEIGHT / SCREEN_WIDTH;
+        // float real_width = mCamera.orthographicSize * 2 * SCREEN_WIDTH / SCREEN_HEIGHT;
+        // float real_height = real_width * SCREEN_HEIGHT / SCREEN_WIDTH;
+        // var posOld = transform.position;
+        // transform.position = new Vector3(posOld.x, offsetY + real_height / 2f, posOld.z);
         var posOld = transform.position;
-        transform.position = new Vector3(posOld.x, offsetY + real_height / 2f, posOld.z);
+        transform.position = new Vector3(posOld.x, offsetY, posOld.z);
     }
  
 }
